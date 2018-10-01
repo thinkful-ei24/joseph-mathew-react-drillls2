916168fa22d025421046358398d35ba9a5bd5bee
Glitch (react-address-book-starting-point) <none>
2018-10-01 11:40:17 -0700

    implement address list

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,4 +2,12 @@ import React from 'react';
 
 import './contact.css';
 
-// The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+  return (
+    <section className="contact">
+      <img className="contact-photo" src={props.photo} />
+      <span className="contact-name">{props.name}</span>
+      <address className="contact-address">{props.address}</address>
+    </section>
+  );
+}
